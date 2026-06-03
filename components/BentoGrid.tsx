8654a22ca371fd5b3726f2c7b833925cd763ae73
Shreya Rao <shreyarao770@gmail.com>
2026-06-03 22:16:48 +0530

    fix HeroTile name

--- components/BentoGrid.tsx
+++ components/BentoGrid.tsx
@@ -2,7 +2,7 @@ import HeroTile from "./HeroTile";
 export default function BentoGrid() {
   return (
     <section className="grid grid-cols-3 gap-4 p-6 w-full">
-      <HeroTile />
+      <HeroTile name="Shreya" />
       
 
       <article className="bg-gray-800 p-6 rounded-xl">

442996fd1b1856eb4c2a541eb06549d68c55ed9c
Shreya Rao <shreyarao770@gmail.com>
2026-06-04 06:53:21 +0530

    Ready for deployment

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,2 +1,18 @@
 import "./globals.css";
 
+export const metadata = {
+  title: "Next Dashboard",
+  description: "My Next.js App",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,3 @@
 export default function Home() {
-  
-      return <h1>Dashboard Working 🚀</h1>
-    
+      return <h1>Dashboard Working 🚀</h1>    
 }
